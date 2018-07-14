9270068c28ac827c1f54d5c73104e80ca6391715
Vaishnav Thakur <vaishnav6887@gmail.com>
2018-07-14 17:03:50 -0500

    Binary tree data structure and Inorder, postorder, preorder traversal techniques.

---
+++ LearningComputerFunda/ComputerFunda.DataStructure/Tree/DSBinaryTree.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.DataStructure.Tree
+{
+    public class DSBinaryTree<T>
+    {
+        public DSTreeNode<T> Head { get; set; }
+
+        public List<T> _TempDataConsole { get; set; }
+
+        public void InOrderTraversal()
+        {
+            this._TempDataConsole = new List<T>();
+            if (this.Head == null)
+            {
+                Console.WriteLine("Tree is empty");
+            }
+            else
+            {
+                Inorder(this.Head);
+            }
+        }
+
+        public void PreOrderTraversal()
+        {
+            this._TempDataConsole = new List<T>();
+            if (this.Head == null)
+            {
+                Console.WriteLine("Tree is empty");
+            }
+            else
+            {
+                Preorder(this.Head);
+            }
+        }
+
+        public void PostOrderTraversal()
+        {
+            this._TempDataConsole = new List<T>();
+            if (this.Head == null)
+            {
+                Console.WriteLine("Tree is empty");
+            }
+            else
+            {
+                Postorder(this.Head);
+            }
+        }
+
+        private void Inorder(DSTreeNode<T> node)
+        {
+            if (node.Left != null)
+            {
+                Inorder(node.Left);
+            }
+
+            _TempDataConsole.Add(node.Data);
+
+            if (node.Right != null)
+            {
+                Inorder(node.Right);
+            }
+        }
+
+        private void Preorder(DSTreeNode<T> node)
+        {
+            _TempDataConsole.Add(node.Data);
+
+            if (node.Left != null)
+            {
+                Preorder(node.Left);
+            }
+
+            if (node.Right != null)
+            {
+                Preorder(node.Right);
+            }
+        }
+
+        private void Postorder(DSTreeNode<T> node)
+        {
+            if(node.Left != null)
+            {
+                Postorder(node.Left);
+            }
+
+            if(node.Right != null)
+            {
+                Postorder(node.Right);
+            }
+
+            _TempDataConsole.Add(node.Data);
+        }
+    }
+}
---
+++ LearningComputerFunda/ComputerFunda.DataStructure/Tree/DSTreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.DataStructure.Tree
+{
+    public class DSTreeNode<T>
+    {
+        public T Data { get; set; }
+        public DSTreeNode<T> Left { get; set; }
+        public DSTreeNode<T> Right { get; set; }
+
+        public DSTreeNode(T item)
+        {
+            this.Data = item;
+        }
+    }
+}
