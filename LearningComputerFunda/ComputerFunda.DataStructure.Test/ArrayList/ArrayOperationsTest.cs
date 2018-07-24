2580726db27c01d2775d9b551bce37642c3431d9
Anshuman Singh <anshumas@gmail.com>
2018-07-24 00:23:26 -0500

    LeftRotation or array

---
+++ LearningComputerFunda/ComputerFunda.DataStructure.Test/ArrayList/ArrayOperationsTest.cs
@@ -0,0 +1,28 @@
+﻿using ComputerFunda.DataStructure.ArrayList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputerFunda.DataStructure.Test.ArrayList
+{
+    [TestClass]
+    public class ArrayOperationsTest
+    {
+        [TestMethod]
+        public void TestLeftRotation_RotationLessThanSize()
+        {
+            int[] intput = new int[] { 1, 2, 3, 4, 5 };
+            int operations = 2;
+            int[] expected = new int[] { 3, 4, 5, 1, 2 };
+            int[] actual = ArrayOperations.LeftRotation(intput, operations);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestLeftRotation_SizeLessThanRotation()
+        {
+            int[] intput = new int[] { 1, 2, 3, 4, 5 };
+            int operations = 6;
+            int[] expected = new int[] { 2, 3, 4, 5, 1 };
+            int[] actual = ArrayOperations.LeftRotation(intput, operations);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LearningComputerFunda/ComputerFunda.DataStructure/ArrayList/ArrayOperations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.DataStructure.ArrayList
+{
+    public static class ArrayOperations
+    {
+        /// <summary>
+        /// https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="d"></param>
+        /// <returns>array</returns>
+        public static int[] LeftRotation(int[] a, int d)
+        {
+            int size = a.Length;
+            if (d > size)
+                d = d % size;
+            int[] result = new int[size];
+            int index = 0;
+            for (int i = d; i < size; i++)
+            {
+                result[index] = a[i];
+                index += 1;
+            }
+            index = size - d;
+            for (int i = 0; i < d; i++)
+            {
+                result[index] = a[i];
+                index += 1;
+            }
+            return result;
+        }
+    }
+}
