ee9f4ac0a92c5a098d525a75c79f2d3c12b9c9f2
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-03-30 01:21:13 -0500

    Selection Sort Implemented

---
+++ LearningComputerFunda/ComputerFunda.Algorithm.Test/Sort/SelectionSortTest.cs
@@ -0,0 +1,42 @@
+﻿using ComputerFunda.Algorithm.Sort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.Algorithm.Test.Sort
+{
+    [TestClass]
+    public class SelectionSortTest
+    {
+        [TestMethod]
+        public void Sort_SelectionSort_Test()
+        {
+            int[] data = new int[] { 64, 25, 12, 22, 11 };
+
+            SelectionSort sort = new SelectionSort();
+
+            int[] actual = sort.Sort(data);
+
+            int[] expected = new int[] { 11, 12, 22, 25, 64 };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Sort_SelectionSortDesc_Test()
+        {
+            int[] data = new int[] { 64, 25, 12, 22, 11 };
+
+            SelectionSort sort = new SelectionSort();
+
+            int[] actual = sort.SortDesc(data);
+
+            int[] expected = new int[] { 64, 25, 22, 12, 11 };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ LearningComputerFunda/ComputerFunda.Algorithm/Sort/SelectionSort.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.Algorithm.Sort
+{
+    public class SelectionSort : ISort<int>
+    {
+        public int[] Sort(int[] arr)
+        {
+            /// Selection sort repeatedly finds the minimum element and placing at the start of the array until array is sorted.
+            /// Selection sort starts with zero index and finds the min number in subarr 0-n if it finds min is not at 0th position then it swaps the min element with the zeroth element.
+            /// Repeates for 1-n, 2-n, ..., n-1 - n.
+
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+                int min = j;
+                for (int i = j + 1; i < arr.Length; i++)
+                {
+                    if (arr[i] < arr[min])
+                    {
+                        min = i;
+                    }
+                }
+
+                if (min != j)
+                {
+                    int swap = arr[j];
+                    arr[j] = arr[min];
+                    arr[min] = swap;
+                }
+            }
+
+            return arr;
+        }
+
+        public int[] SortDesc(int[] arr)
+        {
+            for (int j = 0; j < arr.Length; j++)
+            {
+                int max = j;
+                for (int i = j + 1; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] > arr[max])
+                    {
+                        max = i;
+                    }
+                }
+
+                if (max != j)
+                {
+                    int swap = arr[j];
+                    arr[j] = arr[max];
+                    arr[max] = swap;
+                }
+            }
+
+            return arr;
+        }
+    }
+}
