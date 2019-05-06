57a8a952988d49309bf489b8abfec0b2145c7603
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-06 00:21:58 -0500

    Cracking Coding Interview Book: Problem 1.1: IsUnique char in string

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/String/HasUniqueCharInString.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.String
+{
+    public static class HasUniqueCharInString
+    {
+        public static bool HasUnique(string input)
+        {
+            Dictionary<char, bool> map = new Dictionary<char, bool>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input.ElementAt(i);
+                if (map.ContainsKey(currentChar))
+                {
+                    return false;
+                }
+
+                map.Add(currentChar, true);
+            }
+
+            return true;
+        }
+
+        public static bool HasUniqueWithOutExtraSpace(string input)
+        {
+            bool[] map = new bool[128];
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                int currentChar = (int)input.ElementAt(i);
+
+                if(map[currentChar])
+                {
+                    return false;
+                }
+
+                map[currentChar] = true;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/String/HasUniqueCharInStringTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ComputerFunda.ProgrammingProblem.String;
+
+namespace ProgrammingProblem.Test.String
+{
+    /// <summary>
+    /// Summary description for HasUniqueCharInStringTest
+    /// </summary>
+    [TestClass]
+    public class HasUniqueCharInStringTest
+    {
+        [TestCategory("String")]
+        [TestMethod]
+        public void HasUniqueCharInString_NonUniqueCharTest()
+        {
+            bool actual = HasUniqueCharInString.HasUnique("vaishnav");
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestCategory("String")]
+        [TestMethod]
+        public void HasUniqueCharInString_NonUniqueCharTest_EmptyStringInput()
+        {
+            bool actual = HasUniqueCharInString.HasUnique("");
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestCategory("String")]
+        [TestMethod]
+        public void HasUniqueCharInString_UniqueCharTest()
+        {
+            bool actual = HasUniqueCharInString.HasUnique("Thakur");
+            Assert.IsTrue(actual);
+        }
+
+        [TestCategory("String")]
+        [TestMethod]
+        public void HasUniqueCharInString_UniqueSpecialCharTest()
+        {
+            bool actual = HasUniqueCharInString.HasUnique("!@#$%^&*()");
+            Assert.IsTrue(actual);
+        }
+
+        [TestCategory("String")]
+        [TestMethod]
+        public void HasUniqueCharInString_NonUniqueSpecialCharTest()
+        {
+            bool actual = HasUniqueCharInString.HasUnique("!@#$%^&*(()");
+            Assert.IsFalse(actual);
+        }
+
+
+        [TestCategory("String")]
+        [TestMethod]
+        public void HasUniqueCharInStringWithOutExtraSpace_NonUniqueCharTest()
+        {
+            bool actual = HasUniqueCharInString.HasUniqueWithOutExtraSpace("vaishnav");
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestCategory("String")]
+        [TestMethod]
+        public void HasUniqueCharInStringWithOutExtraSpace_UniqueCharTest()
+        {
+            bool actual = HasUniqueCharInString.HasUniqueWithOutExtraSpace("Thakur");
+            Assert.IsTrue(actual);
+        }
+
+        [TestCategory("String")]
+        [TestMethod]
+        public void HasUniqueCharInStringWithOutExtraSpace_UniqueSpecialCharTest()
+        {
+            bool actual = HasUniqueCharInString.HasUniqueWithOutExtraSpace("!@#$%^&*()");
+            Assert.IsTrue(actual);
+        }
+
+        [TestCategory("String")]
+        [TestMethod]
+        public void HasUniqueCharInStringWithOutExtraSpace_NonUniqueSpecialCharTest()
+        {
+            bool actual = HasUniqueCharInString.HasUniqueWithOutExtraSpace("!@#$%^&*(()");
+            Assert.IsFalse(actual);
+        }
+    }
+}
