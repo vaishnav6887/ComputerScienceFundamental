5f528009beb6f9d23c4022c7779960f189b0d5ba
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-07 22:42:41 -0500

    Cracking coding interview questions : Problems 1.2 Check Permutations for strings.

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/String/CheckStringsArePermutation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.String
+{
+    /// <summary>
+    /// Cracking coding interview : Problem 1.2 Check Permutation: 
+    /// Given two strings, write a method to decide if one is a permutation of the other
+    /// </summary>
+    public static class CheckStringsArePermutation
+    {
+        public static bool CheckPermutation(string argA, string argB)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int index = 0; index < argA.Length; index++)
+            {
+                int currentValue = (int)argA[index];
+                int currentValueTotal = 0;
+
+                if(map.ContainsKey(currentValue))
+                {
+                    currentValueTotal = map[currentValue];
+                }
+                else
+                {
+                    map.Add(currentValue, currentValueTotal);
+                }
+
+                currentValueTotal++;
+
+                map[currentValue] = currentValueTotal;
+            }
+
+            for(int index = 0; index < argB.Length; index++)
+            {
+                int currentValue = (int)argB[index];
+                if(map.ContainsKey(currentValue))
+                {
+                    int cur = map[currentValue] - 1;
+                    
+                    if(cur == 0)
+                    {
+                        map.Remove(currentValue);
+                    }
+                    else
+                    {
+                        map[currentValue] = cur;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return map.Count == 0;
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/String/CheckStringsArePermutationTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ComputerFunda.ProgrammingProblem.String;
+
+namespace ProgrammingProblem.Test.String
+{
+    [TestClass]
+    [TestCategory("String")]
+    public class CheckStringsArePermutationTest
+    {
+        [TestMethod]
+        public void CheckPermutation_Positive()
+        {
+            string argA = "abcdd";
+            string argB = "dbcad";
+
+            Assert.IsTrue(CheckStringsArePermutation.CheckPermutation(argA, argB));
+        }
+
+        [TestMethod]
+        public void CheckPermutation_Negative()
+        {
+            string argA = "abcd";
+            string argB = "dbcad";
+
+            Assert.IsFalse(CheckStringsArePermutation.CheckPermutation(argA, argB));
+        }
+
+        [TestMethod]
+        public void CheckPermutation_Negative_Same_Len()
+        {
+            string argA = "abcdz";
+            string argB = "dbcad";
+
+            Assert.IsFalse(CheckStringsArePermutation.CheckPermutation(argA, argB));
+        }
+    }
+}
