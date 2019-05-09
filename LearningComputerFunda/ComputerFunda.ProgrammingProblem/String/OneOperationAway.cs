b971db825a10b1c0ff72ce36aff3c3650551b02f
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-09 02:40:15 -0500

    Cracking coding interview: Problem 1.5 : One Away (Easy)

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/String/OneOperationAway.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.String
+{
+    public class OneOperationAway
+    {
+        public bool IsOneAway(string argA, string argB)
+        {
+            int max = System.Math.Max(argA.Length, argB.Length) - System.Math.Min(argA.Length, argB.Length);
+
+            if(max > 1)
+            {
+                return false;
+            }
+
+            int a = 0;
+            int b = 0;
+            int skip = 0;
+            while (a < argA.Length && b < argB.Length)
+            {
+                if(argA.ElementAt(a) != argB.ElementAt(b))
+                {
+                    skip++;
+
+                    if(max == 0)
+                    {
+                        b++;
+                    }
+                }
+                else
+                {
+                    b++;
+                }
+
+                a++;
+            }
+
+            return skip <= 1;
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/String/OneOperationAwayTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using ComputerFunda.ProgrammingProblem.String;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingProblem.Test.String
+{
+    [TestCategory("String")]
+    [TestClass]
+    public class OneOperationAwayTest
+    {
+        private OneOperationAway _obj;
+
+        [TestInitialize]
+        public void Init()
+        {
+            this._obj = new OneOperationAway();
+        }
+
+        [TestMethod]
+        public void IsOneAway_Positive()
+        {
+            string argA = "pale";
+            string argB = "ple";
+
+            bool actual = _obj.IsOneAway(argA, argB);
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void IsOneAway_Positive_LastLetterNotMatching()
+        {
+            string argA = "pales";
+            string argB = "pale";
+
+            bool actual = _obj.IsOneAway(argA, argB);
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void IsOneAway_Positive_FirstLetterNotMatching()
+        {
+            string argA = "pale";
+            string argB = "bale";
+
+            bool actual = _obj.IsOneAway(argA, argB);
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void IsOneAway_Negative()
+        {
+            string argA = "pale";
+            string argB = "bake";
+
+            bool actual = _obj.IsOneAway(argA, argB);
+
+            Assert.IsFalse(actual);
+        }
+    }
+}
