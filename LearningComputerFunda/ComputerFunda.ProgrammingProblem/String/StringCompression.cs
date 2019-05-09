9f15a00a49d3f2174f6c4c22d50e5db4aa9c092c
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-09 01:31:48 -0500

    Cracking Coding interview Book: Problem No 1.6: String Compression

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/String/StringCompression.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.String
+{
+    public class StringCompression
+    {
+        public string Compress(string input)
+        {
+            if(input == null)
+            {
+                throw new ArgumentException("Input string can not be null");
+            }
+
+            StringBuilder returnValue = new StringBuilder();
+            int count = 0;
+            char currentChar = input.ElementAt(0);
+            string activeString = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                
+                if(returnValue.Length > input.Length)
+                {
+                    return input;
+                }
+
+                char current = input.ElementAt(i);
+
+                if(current == currentChar)
+                {
+                    StringBuilder str = new StringBuilder();
+                    count++;
+                    activeString = str.AppendFormat("{0}{1}", currentChar, count).ToString();
+                }
+                else
+                {
+                    returnValue = returnValue.AppendFormat("{0}{1}", currentChar, count);
+                    count = 1;
+                }
+
+                currentChar = current;
+            }
+
+            if (activeString.Length > 0)
+            {
+                returnValue = returnValue.Append(activeString);
+            }
+
+            return returnValue.Length <= input.Length ? returnValue.ToString() : input;
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/String/StringCompressionTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ComputerFunda.ProgrammingProblem.String;
+using System;
+
+namespace ProgrammingProblem.Test.String
+{
+    [TestCategory("String")]
+    [TestClass]
+    public class StringCompressionTest
+    {
+        private StringCompression _obj;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _obj = new StringCompression();
+        }
+
+        [TestMethod]
+        public void StringComparission_Positive_Test()
+        {
+            string input = "aabcccccddaaa";
+
+            string actual = this._obj.Compress(input);
+
+            string expected = "a2b1c5d2a3";
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void StringComparission_Negative_Test()
+        {
+            string input = "abcdefgh";
+
+            string actual = this._obj.Compress(input);
+
+            string expected = "a1b1c1d1e1f1g1h1";
+
+            Assert.AreNotEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void StringComparission_Null_Input_Test()
+        {
+            string actual = this._obj.Compress(null);
+
+            Assert.Fail("Input string can not be null");
+        }
+    }
+}
