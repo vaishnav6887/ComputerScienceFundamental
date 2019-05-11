ee91ab14f5858e7eb9b852b1e1447a38eea7b500
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-11 01:56:09 -0500

    Rotate Matrix Anticlock Wise by 90 degrees. Rotate Matrix Anticlock wise by 180 degrees.

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/Array/RotateMatrix.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.Array
+{
+    /// <summary>
+    /// Methods to rotate the matrix by specific degrees.
+    /// </summary>
+    public class RotateMatrix
+    {
+        private int[,] _matrix;
+        private readonly int _length;
+        private readonly int levels;
+
+        public int[,] Matrix
+        {
+            get
+            {
+                return _matrix;
+            }
+        }
+
+        public RotateMatrix(int[,] matrix, int length)
+        {
+            _matrix = matrix;
+            _length = length;
+            levels = _length % 2 == 0 ? _length / 2 : (_length - 1) / 2;
+        }
+
+        public void RotateMatrixBy90DegreeAntiClockWise()
+        {
+            for (int i = 0; i < levels; i++)
+            {
+                for (int j = i; j < (_length - i - 1); j++)
+                {
+                    int swap = _matrix[i, j];
+                    _matrix[i, j] = _matrix[j, _length - i - 1];
+                    _matrix[j, _length - i - 1] = _matrix[_length - i - 1, _length - j - 1];
+                    _matrix[_length - i - 1, _length - j - 1] = _matrix[_length - i - 1, j];
+                    _matrix[_length - i - 1, j] = swap;
+                }
+            }
+        }
+
+        public void RotateMatrixBy180DegreeAnticlockWise()
+        {
+            for (int i = 0; i < levels; i++)
+            {
+                for (int j = i; j < (_length - i - 1); j++)
+                {
+                    int swap = _matrix[i, j];
+                    _matrix[i, j] = _matrix[_length - i - 1, _length - j - 1];
+                    _matrix[_length - i - 1, _length - j - 1] = swap;
+
+                    swap = _matrix[j, _length - i - 1];
+                    _matrix[j, _length - i - 1] = _matrix[_length - j - 1, i];
+                    _matrix[_length - j - 1, i] = swap;
+                }
+            }
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/Array/RotateMatrixTest.cs
@@ -0,0 +1,45 @@
+﻿using ComputerFunda.ProgrammingProblem.Array;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingProblem.Test.Array
+{
+    [TestCategory("Matrix")]
+    [TestClass]
+    public class RotateMatrixTest
+    {
+        private RotateMatrix obj;
+
+        [TestInitialize]
+        public void Init()
+        {
+            int[,] input = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            obj = new RotateMatrix(input, 3);
+        }
+
+        [TestMethod]
+        public void RotateMatrixBy90DegreeAntiClockWise_Positive_Test_3()
+        {
+            obj.RotateMatrixBy90DegreeAntiClockWise();
+
+            int[,] expected = new int[,] { { 3, 6, 9 }, { 2, 5, 8 }, { 1, 4, 7 } };
+
+            Assert.AreEqual(expected[0, 0], obj.Matrix[0, 0]);
+            Assert.AreEqual(expected[0, 2], obj.Matrix[0, 2]);
+            Assert.AreEqual(expected[2, 2], obj.Matrix[2, 2]);
+            Assert.AreEqual(expected[2, 0], obj.Matrix[2, 0]);
+        }
+
+        [TestMethod]
+        public void RotateMatrixBy180DegreeAntiClockWise_Positive_Test_3()
+        {
+            obj.RotateMatrixBy180DegreeAnticlockWise();
+
+            int[,] expected = new int[,] { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
+
+            Assert.AreEqual(expected[0, 0], obj.Matrix[0, 0]);
+            Assert.AreEqual(expected[0, 2], obj.Matrix[0, 2]);
+            Assert.AreEqual(expected[2, 2], obj.Matrix[2, 2]);
+            Assert.AreEqual(expected[2, 0], obj.Matrix[2, 0]);
+        }
+    }
+}
