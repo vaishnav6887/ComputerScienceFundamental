d872046833c1e5a35899f770fb586bbacf03be65
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-11 16:46:17 -0500

    Leet Code: Problem No 203: Remove Linked List Elements

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/LinkedList/RemoveListElement.cs
@@ -0,0 +1,50 @@
+﻿using ComputerFunda.DataStructure.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.LinkedList
+{
+    public class RemoveListElement
+    {
+        private DSNode<int> _head;
+        public RemoveListElement(DSNode<int> head)
+        {
+            _head = head;
+        }
+
+        public DSNode<int> Execute(int val)
+        {
+            if(_head == null)
+            {
+                return _head;
+            }
+
+            DSNode<int> current = _head;
+            DSNode<int> runner = _head.Next;
+
+            while(runner != null)
+            {
+                if(runner.Data == val)
+                {
+                    runner = runner.Next;
+                    current.Next = runner;
+                }
+                else
+                {
+                    current = runner;
+                    runner = runner.Next;
+                }
+            }
+
+            if(_head.Data == val)
+            {
+                return _head.Next;
+            }
+
+            return _head;
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/LinkedList/RemoveListElementTest.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ComputerFunda.ProgrammingProblem.LinkedList;
+using ComputerFunda.DataStructure.LinkedList;
+
+namespace ProgrammingProblem.Test.LinkedList
+{
+    [TestCategory("LinkedList")]
+    [TestClass]
+    public class RemoveListElementTest
+    {
+        private RemoveListElement _data;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var head = new DSNode<int>();
+            head.Data = 1;
+            head.Next = new DSNode<int>()
+            {
+                Data = 2,
+                Next = new DSNode<int>()
+                {
+                    Data = 3,
+                    Next = new DSNode<int>()
+                    {
+                        Data = 4,
+                        Next = new DSNode<int>()
+                        {
+                            Data = 6,
+                            Next = new DSNode<int>()
+                            {
+                                Data = 7,
+                                Next = new DSNode<int>()
+                                {
+                                    Data = 5,
+                                    Next = new DSNode<int>()
+                                    {
+                                        Data = 6
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            _data = new RemoveListElement(head); 
+        }
+
+        [TestMethod]
+        public void Execute_Positive_Test()
+        {
+            int input = 6;
+
+            DSNode<int> actual = _data.Execute(input);
+
+            DSNode<int> expected = new DSNode<int>()
+            {
+                Data = 1,
+                Next = new DSNode<int>()
+                {
+                    Data = 2,
+                    Next = new DSNode<int>()
+                    {
+                        Data = 3,
+                        Next = new DSNode<int>()
+                        {
+                            Data = 4,
+                            Next = new DSNode<int>()
+                            {
+                                Data = 7,
+                                Next = new DSNode<int>()
+                                {
+                                    Data = 5
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            while(expected != null && actual != null)
+            {
+                Assert.AreEqual(expected.Data, actual.Data);
+
+                expected = expected.Next;
+                actual = actual.Next;
+            }
+
+            Assert.IsNull(expected);
+            Assert.IsNull(actual);
+        }
+    }
+}
