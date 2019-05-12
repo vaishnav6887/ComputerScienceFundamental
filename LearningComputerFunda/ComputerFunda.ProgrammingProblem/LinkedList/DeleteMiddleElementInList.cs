2b1481c3aa88b624446073d1583be10bdf0eb631
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-12 01:21:51 -0500

    Cracking Coding Interview: Problem No: 2.3: Delete Middle Node 2 versions: 1st: With out access to middle node. Only have access to Head Node and need to delete middle.
    2nd: With access to middle node. This problem has access to middle nod. Requirement is to delete the incomming middle element. and return new middle.

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/LinkedList/DeleteMiddleElementInList.cs
@@ -0,0 +1,47 @@
+﻿using ComputerFunda.DataStructure.LinkedList;
+
+namespace ComputerFunda.ProgrammingProblem.LinkedList
+{
+    public class DeleteMiddleElementInList
+    {
+        private DSNode<int> _head;
+
+        public DeleteMiddleElementInList(DSNode<int> head)
+        {
+            _head = head;
+        }
+
+        public DSNode<int> Execute()
+        {
+            if(_head == null)
+            {
+                return null;
+            }
+
+            DSNode<int> current = _head;
+            DSNode<int> runner = _head.Next;
+            while(runner != null && runner.Next != null)
+            {
+                runner = runner.Next.Next;
+                if(runner != null && runner.Next != null)
+                {
+                    current = current.Next;
+                }
+            }
+
+            current.Next = current.Next.Next;
+
+            return _head;
+        }
+
+        public DSNode<int> ExecuteWithAccessToMiddleNode(DSNode<int> middle)
+        {
+            if(middle == null || middle.Next == null) { return null; }
+
+            middle.Data = middle.Next.Data;
+            middle.Next = middle.Next.Next;
+
+            return middle;
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/LinkedList/DeleteMiddleElementInListTest.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using ComputerFunda.DataStructure.LinkedList;
+using ComputerFunda.ProgrammingProblem.LinkedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingProblem.Test.LinkedList
+{
+    [TestCategory("LinkedList")]
+    [TestClass]
+    public class DeleteMiddleElementInListTest
+    {
+        private DeleteMiddleElementInList _data;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _data = new DeleteMiddleElementInList(new DSNode<int>()
+            {
+                Data = 3,
+                Next = new DSNode<int>()
+                {
+                    Data = 2,
+                    Next = new DSNode<int>()
+                    {
+                        Data = 3,
+                        Next = new DSNode<int>()
+                        {
+                            Data = 1,
+                            Next = new DSNode<int>()
+                            {
+                                Data = 2,
+                                Next = new DSNode<int>()
+                                {
+                                    Data = 5,
+                                    Next = new DSNode<int>()
+                                    {
+                                        Data = 6,
+                                        Next = new DSNode<int>()
+                                        {
+                                            Data = 6
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        [TestMethod]
+        public void DeleteMiddleElement_Positive()
+        {
+            DSNode<int> expected = new DSNode<int>()
+            {
+                Data = 3,
+                Next = new DSNode<int>()
+                {
+                    Data = 2,
+                    Next = new DSNode<int>()
+                    {
+                        Data = 3,
+                        Next = new DSNode<int>()
+                        {
+                            Data = 2,
+                            Next = new DSNode<int>()
+                            {
+                                Data = 5,
+                                Next = new DSNode<int>()
+                                {
+                                    Data = 6,
+                                    Next = new DSNode<int>()
+                                    {
+                                        Data = 6
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            DSNode<int> actual = _data.Execute();
+
+            while (expected != null && actual != null)
+            {
+                Assert.AreEqual(expected.Data, actual.Data);
+
+                expected = expected.Next;
+                actual = actual.Next;
+            }
+
+            Assert.IsNull(expected);
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void DeleteMiddleElement_WithAccessToMiddleElement_Positive()
+        {
+            DSNode<int> expected = new DSNode<int>()
+            {
+                Data = 2,
+                Next = new DSNode<int>()
+                {
+                    Data = 5,
+                    Next = new DSNode<int>()
+                    {
+                        Data = 6,
+                        Next = new DSNode<int>()
+                        {
+                            Data = 6
+                        }
+                    }
+                }
+            };
+
+            DSNode<int> input = new DSNode<int>()
+            {
+                Data = 1,
+                Next = new DSNode<int>()
+                {
+                    Data = 2,
+                    Next = new DSNode<int>()
+                    {
+                        Data = 5,
+                        Next = new DSNode<int>()
+                        {
+                            Data = 6,
+                            Next = new DSNode<int>()
+                            {
+                                Data = 6
+                            }
+                        }
+                    }
+                }
+            };
+
+            DSNode<int> actual = _data.ExecuteWithAccessToMiddleNode(input);
+
+            while (expected != null && actual != null)
+            {
+                Assert.AreEqual(expected.Data, actual.Data);
+
+                expected = expected.Next;
+                actual = actual.Next;
+            }
+
+            Assert.IsNull(expected);
+            Assert.IsNull(actual);
+        }
+    }
+}
