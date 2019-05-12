615dc0fe884ff982270e630197bc1179ffd72c1f
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-12 00:22:00 -0500

    Cracking coding interview: Problem No: 2.2: Return kth to last element in linkedlist.

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/LinkedList/ReturnKthToLastElement.cs
@@ -0,0 +1,47 @@
+﻿using ComputerFunda.DataStructure.LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.LinkedList
+{
+    public class ReturnKthToLastElement
+    {
+        private DSNode<int> _head;
+
+        public ReturnKthToLastElement(DSNode<int> head)
+        {
+            _head = head;
+        }
+
+        public int Execute(int kthIndex)
+        {
+            if(_head == null)
+            {
+                return -1;
+            }
+
+            int returnValue = -1;
+
+            DSNode<int> runner = _head;
+            DSNode<int> current = _head;
+            int i = 0;
+            while(i < kthIndex && runner.Next != null)
+            {
+                runner = runner.Next;
+                i++;
+            }
+
+            while(runner != null)
+            {
+                runner = runner.Next;
+                current = current.Next;
+                returnValue = current.Data;
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/LinkedList/ReturnKthToLastElementTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using ComputerFunda.DataStructure.LinkedList;
+using ComputerFunda.ProgrammingProblem.LinkedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingProblem.Test.LinkedList
+{
+    [TestClass]
+    [TestCategory("LinkedList")]
+    public class ReturnKthToLastElementTest
+    {
+        private ReturnKthToLastElement _data;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _data = new ReturnKthToLastElement(new DSNode<int>()
+            {
+                Data = 3,
+                Next = new DSNode<int>()
+                {
+                    Data = 2,
+                    Next = new DSNode<int>()
+                    {
+                        Data = 3,
+                        Next = new DSNode<int>()
+                        {
+                            Data = 1,
+                            Next = new DSNode<int>()
+                            {
+                                Data = 2,
+                                Next = new DSNode<int>()
+                                {
+                                    Data = 5,
+                                    Next = new DSNode<int>()
+                                    {
+                                        Data = 6,
+                                        Next = new DSNode<int>()
+                                        {
+                                            Data = 6
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        [TestMethod]
+        public void ReturnKthToLastElement_Positive()
+        {
+            int expected = 5;
+            int input = 3;
+
+            int actual = _data.Execute(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
