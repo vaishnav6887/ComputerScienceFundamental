ddfc5c4e9ed77a2df1a9ce6f173f59d1643866b2
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-14 21:47:20 -0500

    Max Stack in constant time and space.

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/MaxStack.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem
+{
+    public class StackNode
+    {
+        public int Data { get; set; }
+        public int Max { get; set; }
+        public StackNode Next { get; set; }
+
+        public StackNode(int value)
+        {
+            this.Data = value;
+        }
+    }
+
+    public class MaxStack
+    {
+        private StackNode _head;
+
+        public int Count { get; set; }
+
+        public void Push(int val)
+        {
+            var newNode = new StackNode(val);
+            if(this._head == null)
+            {
+                newNode.Max = val;
+                this._head = newNode;
+                this.Count++;
+
+                return;
+            }
+
+            int max = System.Math.Max(val, this._head.Max);
+
+            newNode.Next = _head;
+            newNode.Max = max;
+            _head = newNode;
+            this.Count++;
+        }
+
+        public int Pop()
+        {
+            if(this._head == null)
+            {
+                throw new Exception("Stack is empty");
+            }
+
+            int returnValue = this._head.Data;
+
+            this._head = this._head.Next;
+            this.Count--;
+
+            return returnValue;
+        }
+
+        public int Top()
+        {
+            if (this._head == null)
+            {
+                throw new Exception("Stack is empty");
+            }
+
+            return this._head.Data;
+        }
+
+        public int Max()
+        {
+            if (this._head == null)
+            {
+                throw new Exception("Stack is empty");
+            }
+
+            return this._head.Max;
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/Stack/MaxStackTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ComputerFunda.ProgrammingProblem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingProblem.Test.Stack
+{
+    [TestCategory("Stack")]
+    [TestClass]
+    public class MaxStackTest
+    {
+        [TestMethod]
+        public void MaxStack()
+        {
+            MaxStack node = new MaxStack();
+            node.Push(1);
+            node.Push(5);
+            node.Push(2);
+
+            node.Push(10);
+
+            node.Pop();
+
+            int actual = node.Max();
+            int expected = 5;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
