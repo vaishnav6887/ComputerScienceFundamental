9c0e636ec1e7d7dd1f08510fa5a2231e341b9a21
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-15 23:25:13 -0500

    Sort Stack : Cracking coding interview question 3.5

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/Stack/SortStack.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.Stack
+{
+    public class SortStack
+    {
+        private StackNode _head;
+        private int _count;
+        private StackNode _secondary;
+
+        public SortStack(int[] arr)
+        {
+            int i = arr.Length - 1;
+            while(i >= 0)
+            {
+                StackNode head = new StackNode(arr[i]);
+
+                if(_head == null)
+                {
+                    _head = head;
+                }
+                else
+                {
+                    head.Next = _head;
+                    _head = head;
+                }
+
+                _count++;
+                i--;
+            }
+        }
+
+        public void Push(int val)
+        {
+            var newNode = new StackNode(val);
+            if (this._head == null)
+            {
+                this._head = newNode;
+                this._count++;
+
+                return;
+            }
+
+            newNode.Next = _head;
+            _head = newNode;
+            this._count++;
+        }
+
+        public int Pop()
+        {
+            if (this._head == null)
+            {
+                throw new Exception("Stack is empty");
+            }
+
+            int returnValue = this._head.Data;
+
+            this._head = this._head.Next;
+            this._count--;
+
+            return returnValue;
+        }
+
+        public StackNode Sort()
+        {
+            if(_head == null)
+            {
+                return null;
+            }
+
+            while(_count > 0)
+            {
+                int currentNode = Pop();
+
+                StackNode swap = new StackNode(currentNode);
+
+                while (_secondary != null && _secondary.Data < currentNode)
+                {
+                    Push(_secondary.Data);
+                    _secondary = _secondary.Next;
+                }
+
+                swap.Next = _secondary;
+                _secondary = swap;
+            }
+
+            return _secondary;
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/Stack/SortStackTest.cs
@@ -0,0 +1,46 @@
+﻿using ComputerFunda.ProgrammingProblem.Stack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingProblem.Test.Stack
+{
+    [TestCategory("Stack")]
+    [TestClass]
+    public class SortStackTest
+    {
+        [TestMethod]
+        public void Sort_Stack_Test()
+        {
+            int[] arr = new int[] 
+            {
+                3,
+                5,
+                9,
+                1,
+                24
+            };
+            SortStack sortStack = new SortStack(arr);
+            StackNode actual = sortStack.Sort();
+
+            StackNode expected = new StackNode(1)
+            {
+                Next = new StackNode(3)
+                {
+                    Next = new StackNode(5)
+                    {
+                        Next = new StackNode(9)
+                        {
+                            Next = new StackNode(24)
+                        }
+                    }
+                }
+            };
+
+            while (actual != null)
+            {
+                Assert.AreEqual(actual.Data, expected.Data);
+                actual = actual.Next;
+                expected = expected.Next;
+            }
+        }
+    }
+}
