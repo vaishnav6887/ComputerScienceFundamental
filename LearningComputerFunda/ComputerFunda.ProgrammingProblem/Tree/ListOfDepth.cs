ca1fe3ef8e341f2d7cab4c753b447467adfb4a7b
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-19 00:21:45 -0500

    Cracking Coding Interview : Problem No: 4.3 List of Depth

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/Tree/ListOfDepth.cs
@@ -0,0 +1,51 @@
+﻿using ComputerFunda.DataStructure.LinkedList;
+using ComputerFunda.DataStructure.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.Tree
+{
+    public class ListOfDepth
+    {
+        public DSLinkedList<int>[] levelOrder(DSTreeNode<int> root)
+        {
+            List<DSLinkedList<int>> _node = new List<DSLinkedList<int>>();
+
+            Iterator(root, 0, _node);
+
+            return _node.ToArray();
+        }
+
+        private void Iterator(DSTreeNode<int> root, int level, List<DSLinkedList<int>> _node)
+        {
+            if(root == null)
+            {
+                return;
+            }
+
+            if(_node.Count == level)
+            {
+                var list = new DSLinkedList<int>();
+                list.Add(root.Data);
+                _node.Add(list);
+            }
+            else
+            {
+                _node[level].Add(root.Data);
+            }
+
+            if(root.Left != null)
+            {
+                Iterator(root.Left, level + 1, _node);
+            }
+
+            if (root.Right != null)
+            {
+                Iterator(root.Right, level + 1, _node);
+            }
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/Tree/ListOfDepthTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using ComputerFunda.DataStructure.LinkedList;
+using ComputerFunda.DataStructure.Tree;
+using ComputerFunda.ProgrammingProblem.Tree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingProblem.Test.Tree
+{
+    [TestClass]
+    [TestCategory("BinaryTree")]
+    public class ListOfDepthTest
+    {
+        [TestMethod]
+        public void Iterator_Test()
+        {
+            DSTreeNode<int> dSTreeNode = new DSTreeNode<int>(1);
+            dSTreeNode.Left = new DSTreeNode<int>(2)
+            {
+                Right = new DSTreeNode<int>(5),
+                Left = new DSTreeNode<int>(4)
+            };
+
+            dSTreeNode.Right = new DSTreeNode<int>(3)
+            {
+                Right = new DSTreeNode<int>(7),
+                Left = new DSTreeNode<int>(6)
+            };
+
+            ListOfDepth obj = new ListOfDepth();
+            var actual = obj.levelOrder(dSTreeNode);
+            var expected = new DSLinkedList<int>[3];
+            expected[0] = new DSLinkedList<int>();
+            expected[0].Add(1);
+
+            expected[1] = new DSLinkedList<int>();
+            expected[1].Add(2);
+            expected[1].Add(3);
+
+            expected[2] = new DSLinkedList<int>();
+            expected[2].Add(4);
+            expected[2].Add(5);
+            expected[2].Add(6);
+            expected[2].Add(7);
+
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(actual.Length, expected.Length);
+
+            for (int i = 0; i < actual.Length; i++)
+            {
+                var actualData = actual[i]._head;
+                var expectedData = expected[i]._head;
+
+                Assert.AreEqual(actual[i].Count, expected[i].Count);
+
+                while(actualData != null)
+                {
+                    Assert.AreEqual(actualData.Data, expectedData.Data);
+
+                    actualData = actualData.Next;
+                    expectedData = expectedData.Next;
+                }
+            }
+        }
+    }
+}
