dee35c740b389193a635109d72ecfbd2a079733a
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-24 00:26:00 -0500

    InsertionSort Algorithm implemented

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/Sorting/InsertionSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.Sorting
+{
+    public class InsertionSort : ISort
+    {
+        private int[] arr;
+
+        public InsertionSort(int[] _arr)
+        {
+            arr = _arr;
+        }
+
+        public int[] Sort()
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int current = arr[i];
+                int j = i - 1;
+                int cachedI = i;
+                for (; j >= 0; j--)
+                {
+                    if(current < arr[j])
+                    {
+                        arr[cachedI] = arr[j];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    cachedI--;
+                }
+
+                arr[j + 1] = current;
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/Sorting/InsertionSortTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using ComputerFunda.ProgrammingProblem.Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingProblem.Test.Sorting
+{
+    [TestCategory("Sorting")]
+    [TestClass]
+    public class InsertionSortTest
+    {
+        [TestMethod]
+        public void InsertionSortTest_Positive()
+        {
+            int Min = 0;
+            int Max = 20;
+            Random randNum = new Random();
+            int[] input = Enumerable
+                .Repeat(Min, Max)
+                .Select(i => randNum.Next(Min, Max)).OrderByDescending(s => s).Distinct()
+                .ToArray();
+
+            ISort sort = new InsertionSort(input);
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            int[] actual = sort.Sort();
+            timer.Stop();
+            input = input.OrderBy(s => s).ToArray();
+
+            for (int expected = 0; expected < input.Length; expected++)
+            {
+                Assert.AreEqual(input[expected], actual[expected]);
+            }
+        }
+    }
+}
