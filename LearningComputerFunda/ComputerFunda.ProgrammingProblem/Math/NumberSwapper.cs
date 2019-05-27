e83497a681b2b3f3157dd520793792ad70db773a
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-27 00:33:30 -0500

    Cracking Coding Interview : Problem No: 16.1 Number Swapper

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/Math/NumberSwapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.Math
+{
+    public class NumberSwapper
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+
+        public NumberSwapper(int num1, int num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+        }
+
+        public void SwapInPlace()
+        {
+            Num1 = Num1 + Num2;
+            Num2 = Num1 - Num2;
+            Num1 = Num1 - Num2;
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/Math/NumberSwapperTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ComputerFunda.ProgrammingProblem.Math;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingProblem.Test.Math
+{
+    [TestClass]
+    [TestCategory("Math")]
+    public class NumberSwapperTest
+    {
+        [TestMethod]
+        public void Swap_Test()
+        {
+            NumberSwapper s = new NumberSwapper(5, -2);
+
+            s.SwapInPlace();
+
+            Assert.AreEqual(s.Num2, 5);
+            Assert.AreEqual(s.Num1, -2);
+        }
+    }
+}
