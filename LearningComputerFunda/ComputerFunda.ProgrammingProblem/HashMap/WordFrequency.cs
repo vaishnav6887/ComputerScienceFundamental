45100f24e9566b1f6075bddd3e5db1f9e0b99de7
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-05-28 12:11:45 -0500

    Cracking Coding interview : 16.2 Word Frequencies

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/HashMap/WordFrequency.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.HashMap
+{
+    public class WordFrequency
+    {
+        private string[] Book { get; }
+
+        private Dictionary<string, int> _map;
+
+        public WordFrequency(string[] book)
+        {
+            Book = book;
+
+            _map = new Dictionary<string, int>();
+        }
+
+        public int FindWordFrequency(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                throw new ArgumentNullException("Word to be searched can not be null or empty.");
+            }
+
+            if (_map.ContainsKey(word))
+            {
+                return _map[word];
+            }
+            else
+            {
+                int frequency = 0;
+
+                frequency = Book.Count(s => s.Equals(word));
+
+                _map.Add(word, frequency);
+
+                return frequency;
+            }
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/HashMap/WordFrequencyTest.cs
@@ -0,0 +1,54 @@
+﻿using ComputerFunda.ProgrammingProblem.HashMap;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingProblem.Test.HashMap
+{
+    [TestCategory("HashMap")]
+    [TestClass]
+    public class WordFrequencyTest
+    {
+        [TestMethod]
+        public void FindFrequency_EmptyBook_Test()
+        {
+            WordFrequency obj = new WordFrequency("".Split());
+            int actual = obj.FindWordFrequency("book");
+
+            int expected = 0;
+
+            Assert.IsNotNull(obj);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FindFrequency_EmptyWord_Test()
+        {
+            WordFrequency obj = new WordFrequency("This is test".Split());
+            int actual = obj.FindWordFrequency("");
+        }
+
+        [TestMethod]
+        public void FindFrequency_Test()
+        {
+            WordFrequency obj = new WordFrequency("I am book, I am book on Programing. You can book my early edition in book stores. I am sure I will be soon your favorite book".Split());
+            Stopwatch watch = new Stopwatch();
+            int expected = 5;
+            watch.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                int actual = obj.FindWordFrequency("book");
+
+                Assert.IsNotNull(obj);
+                Assert.AreEqual(expected, actual);
+            }
+            
+            watch.Stop();
+        }
+    }
+}
