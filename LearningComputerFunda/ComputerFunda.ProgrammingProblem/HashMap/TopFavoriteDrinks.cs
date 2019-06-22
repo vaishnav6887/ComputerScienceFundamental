3509b2cd3978c787217d77d9cd979f070f5359b3
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-06-22 18:10:05 -0500

    Lazy Bar Tender (Find TopFavoriteDrinks) (Daily Programming Question)

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/HashMap/TopFavoriteDrinks.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ComputerFunda.ProgrammingProblem.HashMap
+{
+    /// <summary>
+    /// At a popular bar, each customer has a set of favorite drinks, and will happily accept any rink among this set. 
+    /// For example, in the folllowing situation, customer 0 will be satified with drinks 0, 1, 3, or 6.
+    /// Preferences: {
+    /// 0: [0, 1, 3, 6],
+    /// 1: [1,4,7],
+    /// 2: [2,4,7,5],
+    /// 3: [3,2,5],
+    /// 4: [5,8]
+    /// 
+    /// Lazy bar tender is trying to reduce his efforts by limiting the drinkshe must learn in order to satisfy all customers.
+    /// For the input above, the answer would be 2, as drinks 1 and 5 will satisfy everyone.
+    /// </summary>
+    public class TopFavoriteDrinks
+    {
+        private int[] FindFavoriteDrinks(Dictionary<int, int[]> customerPrefrences)
+        {
+            List<int> returnValue = new List<int>();
+
+            Dictionary<int, HashSet<int>> map = new Dictionary<int, HashSet<int>>();
+
+            HashSet<int> uniqueCust = new HashSet<int>();
+
+            foreach(var data in customerPrefrences)
+            {
+                foreach(var pref in data.Value)
+                {
+                    if (map.ContainsKey(pref))
+                    {
+                        map[pref].Add(data.Key);
+                    }
+                    else
+                    {
+                        map[pref] = new HashSet<int>() { data.Key };
+                    }
+
+                    uniqueCust.Add(data.Key);
+                }
+            }
+
+            var items = map.OrderByDescending(s => s.Value.Count);
+            int i = 0;
+            while (items.Count() > i)
+            {
+                var current = items.ElementAt(i);
+
+                bool isRequired = false;
+
+                foreach (var item in current.Value)
+                {
+                    if (uniqueCust.Contains(item))
+                    {
+                        isRequired = true;
+                        break;
+                    }
+                }
+
+                uniqueCust.RemoveWhere(s => current.Value.Contains(s));
+
+                if (isRequired)
+                {
+                    returnValue.Add(current.Key);
+                }
+
+                i++;
+            }
+
+            return returnValue.ToArray();
+        }
+
+        public int CountOfFavoriteDrinks(Dictionary<int, int[]> customerPrefrences)
+        {
+            return FindFavoriteDrinks(customerPrefrences).Length;
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/HashMap/TopFavoriteDrinksTest.cs
@@ -0,0 +1,62 @@
+﻿using ComputerFunda.ProgrammingProblem.HashMap;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ProgrammingProblem.Test.HashMap
+{
+    [TestCategory("HashMap")]
+    [TestClass]
+    public class TopFavoriteDrinksTest
+    {
+        [TestMethod]
+        public void TestFindFavDrink()
+        {
+            Dictionary<int, int[]> data = new Dictionary<int, int[]>();
+            data.Add(0, new int[] { 0, 1, 3, 6 });
+            data.Add(1, new int[] { 1, 4, 7 });
+            data.Add(2, new int[] { 2, 4, 7, 5 });
+            data.Add(3, new int[] { 3, 2, 5 });
+            data.Add(4, new int[] { 5, 8 });
+
+            TopFavoriteDrinks obj = new TopFavoriteDrinks();
+            int actual = obj.CountOfFavoriteDrinks(data);
+            int expected = 2;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void TestFindFavDrink_All_UniqueChoices()
+        {
+            Dictionary<int, int[]> data = new Dictionary<int, int[]>();
+            data.Add(0, new int[] { 0, 1});
+            data.Add(1, new int[] { 2, 3, 4 });
+            data.Add(2, new int[] { 5, 6 });
+            data.Add(3, new int[] { 10, 9, 8 });
+            data.Add(4, new int[] { 7, 11 });
+
+            TopFavoriteDrinks obj = new TopFavoriteDrinks();
+            int actual = obj.CountOfFavoriteDrinks(data);
+            int expected = 5;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        public void TestFindFavDrink_All_SameChoices()
+        {
+            Dictionary<int, int[]> data = new Dictionary<int, int[]>();
+            data.Add(0, new int[] { 0, 1 });
+            data.Add(1, new int[] { 1, 3, 4 });
+            data.Add(2, new int[] { 5, 1 });
+            data.Add(3, new int[] { 10, 1, 8 });
+            data.Add(4, new int[] { 7, 1 });
+
+            TopFavoriteDrinks obj = new TopFavoriteDrinks();
+            int actual = obj.CountOfFavoriteDrinks(data);
+            int expected = 1;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
