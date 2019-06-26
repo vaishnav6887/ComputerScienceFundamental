15a86e60ab58cde91e0b24ac995da391af2126b4
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-06-25 22:35:11 -0500

    CombinationSum Not completed

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/BackTracking/CombinationSum.cs
@@ -0,0 +1,32 @@
+﻿using ComputerFunda.ProgrammingProblem.Sorting;
+using System.Collections.Generic;
+
+namespace ComputerFunda.ProgrammingProblem.BackTracking
+{
+    public class CombinationSum
+    {
+        private int[] _arr;
+        private ISort _sortHelper;
+        private List<int[]> consoleResult = new List<int[]>();
+
+        public CombinationSum(int[] arr, ISort sortHelper)
+        {
+            _arr = arr;
+            _sortHelper = sortHelper;
+        }
+
+        public List<int[]> FindCombinationSum(int sum)
+        {
+            _arr = _sortHelper.Sort(_arr);
+            List<int> v = new List<int>();
+            PerformUnitWork(sum, _arr.Length, v);
+
+            return consoleResult;
+        }
+
+        private void PerformUnitWork(int sum, int n, List<int> v)
+        {
+            
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/BackTracking/CombinationSumTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using ComputerFunda.ProgrammingProblem.BackTracking;
+using ComputerFunda.ProgrammingProblem.Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingProblem.Test.BackTracking
+{
+    [TestClass]
+    public class CombinationSumTest
+    {
+        [TestMethod]
+        public void FindCombinationSum_Test()
+        {
+            int[] arr = new int[] { 10, 1, 2, 7, 6, 1, 5 };
+            ISort sort = new QuickSort();
+            CombinationSum obj = new CombinationSum(arr, sort);
+            var actual = obj.FindCombinationSum(8);
+            List<int[]> expected = new List<int[]>()
+            {
+                new int[] { 1, 7 },
+                new int[] { 1, 2, 5 },
+                new int[] { 2, 6 },
+                new int[] { 1, 1, 6 }
+            };
+        }
+    }
+}
