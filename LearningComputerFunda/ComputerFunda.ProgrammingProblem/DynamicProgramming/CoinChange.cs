5ecf40aecdce451ad9494065bed425ad1a590c12
Vaishnav Thakur <vaishnav6887@gmail.com>
2019-07-28 16:00:03 -0500

    Coin Change Problem

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/DynamicProgramming/CoinChange.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.DynamicProgramming
+{
+    public class CoinChange
+    {
+        public int GetMeChange(int money, int n)
+        {
+            Dictionary<int, int> memorization = new Dictionary<int, int>();
+            memorization.Add(0, 0);
+            return MinCountForChange(money, memorization, n);
+        }
+
+        private int MinCountForChange(int money, Dictionary<int, int> memorization, int n)
+        {
+            if (memorization.ContainsKey(money))
+            {
+                return memorization[money];
+            }
+            else
+            {
+                int returnMoney = Int32.MaxValue;
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i <= money)
+                        returnMoney = System.Math.Min(returnMoney, MinCountForChange(money - i, memorization, n)) + 1;
+                }
+
+                memorization.Add(money, returnMoney);
+                return returnMoney;
+            }
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/DynamicProgramming/CoinChangeTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ComputerFunda.ProgrammingProblem.DynamicProgramming;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingProblem.Test.DynamicProgramming
+{
+    [TestCategory("DynamicProgramming")]
+    [TestClass]
+    public class CoinChangeTest
+    {
+        [TestMethod]
+        public void GetMeChange_PositiveTest()
+        {
+            CoinChange coinChange = new CoinChange();
+
+            int actual = coinChange.GetMeChange(611, 5);
+            int expected = 123;
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
