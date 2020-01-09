894e96f4fa008362a0f93dc4ab40e085ee05587a
Vaishnav Thakur <vaishnav6887@gmail.com>
2020-01-09 00:19:50 -0600

    Hacker Rank : FB13: Height of the Binary Tree

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/Tree/HeightOfBinaryTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerFunda.ProgrammingProblem.Tree
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+    public class HeightOfBinaryTree
+    {
+        public int FindHeight(Node head)
+        {
+            return InOrderTraversal(head);
+        }
+
+        public int InOrderTraversal(Node node)
+        {
+            int response = 1;
+            int left = 0;
+            int right = 0;
+            if(node.Left != null)
+            {
+                left = InOrderTraversal(node.Left);
+            }
+
+            if(node.Right != null)
+            {
+                right = InOrderTraversal(node.Right);
+            }
+
+            return response + System.Math.Max(left, right);
+        }
+    }
+}
