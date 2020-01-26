685b8175c8db53c22693469709b74b41a4f07f12
Vaishnav Thakur <vaishnav6887@gmail.com>
2020-01-25 19:36:53 -0600

    Leet Code: Problem No: 24. Swap Nodes in Pairs (Medium)

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/LinkedList/SwapPairs.cs
@@ -0,0 +1,57 @@
+﻿using ComputerFunda.DataStructure.LinkedList;
+
+namespace ComputerFunda.ProgrammingProblem.LinkedList
+{
+    public class SwapPairs
+    {
+        private DSNode<int> _head;
+
+        public SwapPairs(DSNode<int> head)
+        {
+            _head = head;
+        }
+
+        public DSNode<int> Swap()
+        {
+            if (_head == null)
+                return null;
+
+            if (_head.Next == null)
+                return _head;
+
+            var current = _head;
+            DSNode<int> prev = null;
+            var next = _head.Next;
+            DSNode<int> newHead = null;
+
+            while (current != null && next != null)
+            {
+                DSNode<int> cached = this.SwapHelper(current, next, ref prev, ref newHead);
+
+                current = cached;
+
+                if (current != null)
+                    next = current.Next;
+            }
+
+            return newHead;
+        }
+
+        private DSNode<int> SwapHelper(DSNode<int> current, DSNode<int> next, ref DSNode<int> prev, ref DSNode<int> newHead)
+        {
+            var cached = next.Next;
+            next.Next = current;
+            current.Next = cached;
+
+            if (prev != null)
+                prev.Next = next;
+
+            prev = current;
+
+            if (newHead == null)
+                newHead = next;
+
+            return cached;
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/LinkedList/SwapPairsTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using ComputerFunda.DataStructure.LinkedList;
+using ComputerFunda.ProgrammingProblem.LinkedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingProblem.Test.LinkedList
+{
+    [TestClass]
+    public class SwapPairsTest
+    {
+        [TestCategory("LinkedList")]
+        [TestMethod]
+        public void SwapTest()
+        {
+            DSNode<int> head = new DSNode<int>();
+            head.Data = 1;
+            head.Next = new DSNode<int>()
+            {
+                Data = 2,
+                Next = new DSNode<int>()
+                {
+                    Data = 3,
+                    Next = new DSNode<int>()
+                    {
+                        Data = 4
+                    }
+                }
+            };
+
+            DSNode<int> expected = new DSNode<int>();
+            expected.Data = 2;
+            expected.Next = new DSNode<int>()
+            {
+                Data = 1,
+                Next = new DSNode<int>()
+                {
+                    Data = 4,
+                    Next = new DSNode<int>()
+                    {
+                        Data = 3
+                    }
+                }
+            };
+
+            SwapPairs pairs = new SwapPairs(head);
+
+            DSNode<int> actual =  pairs.Swap();
+
+            while(actual.Next != null && expected.Next != null)
+            {
+                Assert.AreEqual(actual.Data, expected.Data);
+                actual = actual.Next;
+                expected = expected.Next;
+            }
+        }
+    }
+}
