fa91a498f93101f90f2607ffe86343d55d2b4bdd
Vaishnav Thakur <vaishnav6887@gmail.com>
2020-01-29 20:30:50 -0600

    KnapSnack Problem implementation and its test case. Dynamic programming getting started.

---
+++ LearningComputerFunda/ComputerFunda.ProgrammingProblem/DynamicProgramming/knapSnack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace ComputerFunda.ProgrammingProblem.DynamicProgramming
+{
+    public class KnapSnack
+    {
+        public List<int> _weights { get; set; }
+        public List<int> _profits { get; set; }
+        public int _capacity { get; set; }
+        public int _maxProfit { get; set; }
+        public KnapSnack(List<int> weights, List<int> profits, int capacity)
+        {
+            _capacity = capacity;
+            _weights = weights;
+            _profits = profits;
+            _maxProfit = 0;
+        }
+
+        public int Calculate(int remainingCapacity, int currentWeight = 0, int currentProfit = 0, int index = 0)
+        {
+            if(remainingCapacity <= 0 || index >= _weights.Count)
+            {
+                return currentProfit;
+            }
+
+            int newWeight = currentWeight + _weights[index];
+
+            int withCurrent = 0;
+            int withOutCurrent = 0;
+
+            if(newWeight <= _capacity)
+                withCurrent = Calculate(remainingCapacity - _weights[index], newWeight, currentProfit + _profits[index], index + 1);
+
+            withOutCurrent = Calculate(remainingCapacity, currentWeight, currentProfit, index + 1);
+
+            _maxProfit = System.Math.Max(_maxProfit, System.Math.Max(withCurrent, withOutCurrent));
+
+            return System.Math.Max(withCurrent, withOutCurrent);
+        }
+    }
+}
---
+++ LearningComputerFunda/ProgrammingProblem.Test/DynamicProgramming/KnapSnackTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ComputerFunda.ProgrammingProblem.DynamicProgramming;
+using System.Collections.Generic;
+
+namespace ProgrammingProblem.Test.DynamicProgramming
+{
+    [TestClass]
+    public class KnapSnackTest
+    {
+        [TestCategory("DynamicProgramming")]
+        [TestMethod]
+        public void Test_KnapSnack_Calculate()
+        {
+            List<int> weights = new List<int>() { 2, 3, 1, 4 };
+            List<int> profits = new List<int>() { 4, 5, 3, 7 };
+            int capacity = 5;
+
+            KnapSnack obj = new KnapSnack(weights, profits, capacity);
+            obj.Calculate(capacity);
+            int expected = 10;
+
+            Assert.AreEqual(expected, obj._maxProfit);
+        }
+    }
+}
